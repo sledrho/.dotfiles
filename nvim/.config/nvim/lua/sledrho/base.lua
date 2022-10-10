f89430c76457100a33c0f1fd340c661a7b0e9e67
Rory Sheldon <rory.sheldon@fundingcircle.com>
2022-10-10 15:28:28 +0100

    adding base settings

---
+++ nvim/.config/nvim/lua/sledrho/base.lua
@@ -0,0 +1,26 @@
+-- Basic Settings
+local set = vim.opt
+
+-- Tab behaviour
+set.shiftwidth = 4
+set.tabstop = 4
+set.softtabstop = 4
+set.expandtab = true
+
+
+set.smartindent = true
+set.guicursor = true
+set.relativenumber = true
+set.nu = true
+set.nohlsearch = true
+set.hidden = true
+set.noerrorbells = true
+set.nowrap = true
+set.noswapfile = true
+set.nobackup = true
+
+set.scrolloff = 8
+set.colorcolumn = 85
+
+set.cmdheight = 2
+set.mouse = 'a'
