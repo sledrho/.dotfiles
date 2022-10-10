3fa6335b5dfe854355f956cf8bef1f4600fa66c2
Rory Sheldon <rory.sheldon@fundingcircle.com>
2022-10-10 14:55:14 +0100

    more plugins

--- nvim/.config/nvim/lua/sledrho/plugins.lua
+++ nvim/.config/nvim/lua/sledrho/plugins.lua
@@ -1,11 +1,28 @@
 -- Plugins
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+  use "wbthomason/packer.nvim"
+  use "nvim-lua/plenary.nvim"
 
   -- Git Related Plugins
   use "tpope/vim-fugitive"
   use "airblade/vim-gitgutter"
+
+  -- UI
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  }
+  use {
+   'nvim-lualine/lualine.nvim',
+   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  -- Languages/Syntax
+  use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
 
 
   -- Themes
