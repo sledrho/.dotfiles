f0234cbc5a003c4f64473ed3956043991b215763
Rory Sheldon <rory.sheldon006@gmail.com>
2023-07-02 12:30:05 +0000

    add packer bootstrap snipper

--- nvim/.config/nvim/lua/sledrho/plugins.lua
+++ nvim/.config/nvim/lua/sledrho/plugins.lua
@@ -1,7 +1,23 @@
 -- Plugins
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use "wbthomason/packer.nvim"
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
   use "nvim-lua/plenary.nvim"
 
   -- Git Related Plugins
@@ -68,6 +84,13 @@ return require('packer').startup(function(use)
       "windwp/nvim-autopairs",
       config = function() require("nvim-autopairs").setup {} end
   }
+  use {
+        "danymat/neogen",
+        config = function()
+            require('neogen').setup {}
+        end,
+        dependencies = "nvim-treesitter/nvim-treesitter",
+  }
   -- vscode like peek
   use({
     "dnlhc/glance.nvim",
@@ -130,7 +153,10 @@ return require('packer').startup(function(use)
   -- Themes
   use "EdenEast/nightfox.nvim" -- Packer
   use "folke/tokyonight.nvim"
-
-
-
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
+
