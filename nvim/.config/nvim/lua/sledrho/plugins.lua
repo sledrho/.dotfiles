c51cb1138cc52c5a4fa474b36a634bc65f7c9cc0
Rory Sheldon <rory.sheldon@fundingcircle.com>
2023-09-15 11:50:11 +0100

    Add correct telescope binding & fix tmux timeouts

--- nvim/.config/nvim/lua/sledrho/plugins.lua
+++ nvim/.config/nvim/lua/sledrho/plugins.lua
@@ -139,6 +139,8 @@ return require('packer').startup(function(use)
   use {
     "folke/which-key.nvim",
     config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
       require("which-key").setup {
         -- your configuration comes here
         -- or leave it empty to use the default settings
